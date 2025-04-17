45748222748303e8a9e6c99f49a0832c60d96b9e
Nicolas <nicolasd@berkeley.edu>
2025-04-17 14:48:00 -0700

    Initial Commit

---
+++ bouncy/bouncyApp.swift
@@ -0,0 +1,17 @@
+//
+//  bouncyApp.swift
+//  bouncy
+//
+//  Created by Nico on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct bouncyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bouncyTests/bouncyTests.swift
@@ -0,0 +1,17 @@
+//
+//  bouncyTests.swift
+//  bouncyTests
+//
+//  Created by Nico on 4/17/25.
+//
+
+import Testing
+@testable import bouncy
+
+struct bouncyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
